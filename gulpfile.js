16a6b254d592ef47392c7289bf1c01edafe97aa3
Andrei Ovchinnikov <andrey-1983@mail.ru>
2021-05-28 15:39:56 +1000

    added and adjusted promo section

--- gulpfile.js
+++ gulpfile.js
@@ -15,17 +15,17 @@ gulp.task('server', function() {
 });
 
 gulp.task('styles', function() {
-   return gulp.src("src/sass/*.+(scss|sass)")
+   return gulp.src("src/sass/**/*.+(scss|sass)")
             .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
             .pipe(rename({suffix: ".min"}))
-            .pipe(autoprefixer({cascade: false}))
+            .pipe(autoprefixer())
             .pipe(cleanCSS({compatibility: 'ie8'}))
             .pipe(gulp.dest("src/css"))
             .pipe(browserSync.stream());
 });
 
 gulp.task('watch', function() {
-   gulp.watch("src/sass/*.+(scss|sass)", gulp.parallel("styles"));
+   gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel("styles"));
    gulp.watch("src/*.html").on("change", browserSync.reload);
 });
 
