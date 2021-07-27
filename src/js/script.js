61173a2706f19208e9001dc60757df83ed61ad45
Andrei Ovchinnikov <andrei.s.ovchinnikov@gmail.com>
2021-07-27 16:25:26 +1000

    pageup

--- src/js/script.js
+++ src/js/script.js
@@ -100,4 +100,19 @@ $(document).ready(function(){
 		});
 		return false;
 	 });
+
+	 // Smooth scroll and pageup
+	 $(window).scroll(function() {
+		if($(this).scrollTop() > 1600) {
+			$('.pageup').fadeIn();
+		} else {
+			$('.pageup').fadeOut();
+		}
+	 });
+
+	 $("a[href^='#']").click(function(){
+		 const _href= $(this).attr("href");
+		 $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+		 return false;
+	 })
  });
