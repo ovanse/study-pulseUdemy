727d18e83b45df1fdfec9c8b08dc416ac2ce3624
Andrei Ovchinnikov <andrei.s.ovchinnikov@gmail.com>
2021-07-29 13:35:48 +1000

    animate.css & wow.js

--- src/js/script.js
+++ src/js/script.js
@@ -114,5 +114,6 @@ $(document).ready(function(){
 		 const _href= $(this).attr("href");
 		 $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
 		 return false;
-	 })
+	 });
+	  new WOW().init();
  });
